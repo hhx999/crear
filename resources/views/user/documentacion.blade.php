f49a674000e7bdd2c9ef1abbea8c309201c2ec5b
hhx999 <borjas.vegariveras@gmail.com>
2019-06-03 12:31:06 -0300

    TG-183 #closed Funcion DOCUMENTACIÓN agregada

--- resources/views/user/documentacion.blade.php
+++ resources/views/user/documentacion.blade.php
@@ -4,29 +4,25 @@ use App\Libraries\Helpers;
 <!DOCTYPE html>
 <html>
 <head>
-<title>MENÚ USUARIO - LINEA EMPRENDEDOR</title>
- <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel='stylesheet' href="<?=$GLOBALS['raiz'].'resources/assets/css/fuente.css'?>">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css' integrity='sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ' crossorigin='anonymous'>
-  <link rel="icon" href="favicon.ico" type="image/x-icon"/>
-  <link rel="shortcut icon" href="<?=$GLOBALS['raiz'].'resources/assets/img/logo.svg'?>" type="image/x-icon"/>
-  
-  <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
-  <link rel="stylesheet" type="text/css" href="<?=$GLOBALS['raiz'].'resources/assets/css/style.css'?>">
-  <link rel="stylesheet" type="text/css" href="<?=$GLOBALS['raiz'].'resources/assets/css/alertify.css'?>">
+	<title>MENÚ USUARIO - LINEA EMPRENDEDOR</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <script type="text/javascript" src="<?=$GLOBALS['raiz'].'resources/assets/js/alertify.min.js'?>"></script>
-  <script src="<?=$GLOBALS['raiz'].'resources/assets/js/jquery.min.js'?>" type="text/javascript"></script>
+	<link rel="stylesheet" href="{{ asset('css/w3.css') }}">
+	<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+	<link rel="stylesheet" href="{{ asset('css/alertify.css') }}">
+	<script type="text/javascript" src="{{ asset('js/jquery.min.js') }}"></script>
+	<script type="text/javascript" src="{{ asset('js/jquery.mask.min.js') }}"></script>
+	<script type="text/javascript" src="{{ asset('js/jquery.validate.min.js') }}"></script>
+	<script type="text/javascript" src="{{ asset('js/alertify.min.js') }}"></script>
+
+	<link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css' integrity='sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ' crossorigin='anonymous'>
+	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
 </head>
 <body>
 
   <div class="w3-col m1 w3-center"><p> </p></div>
   <div class="w3-col m10 w3-white w3-center">
-      <div style="margin-bottom: 30px;">
-        <img src="<?=$GLOBALS['raiz'].'resources/assets/img/logo_crear.jpg'?>">
-      </div>
-        <a href="<?=$GLOBALS['urlRaiz'].'user'?>"><button type="button">Volver a FORMULARIOS</button></a><br>
+        <a href="{{ url('/user') }}"><button type="button">Volver a FORMULARIOS</button></a><br>
 <br>
 <input type="text" id="success" value="<?php if ($msg == '1') { echo $msg; } else if ($msg == '0') {echo $msg;} else { echo '2';} ?>" hidden>
   <div class="w3-panel w3-pale-green w3-leftbar w3-border-light-green">
@@ -56,7 +52,7 @@ use App\Libraries\Helpers;
 		cursor: pointer;
 	}
 </style>
-<form action="<?=$GLOBALS['urlRaiz'].'agregarDocumentacion/'.$id?>" method="post" enctype="multipart/form-data">
+<form action="{{ url('/agregarDocumentacion/'.$id) }}" method="post" enctype="multipart/form-data">
 	<div class="w3-row">
 	  <div class="w3-col w3-container m4 l3">
 	    <select name="descripcion" id="desc">
@@ -111,7 +107,7 @@ use App\Libraries\Helpers;
 					'.$documentacion[$i]->multimedia_id.'
 					</td>
 					<td>
-				      <img class="w3-bar-item w3-hide-small" style="width:85px" src="'.$GLOBALS['raiz'].'storage'.DIRECTORY_SEPARATOR.'img'.DIRECTORY_SEPARATOR.$multimedia->id.'">
+				      <img class="w3-bar-item w3-hide-small" style="width:85px" src='.asset("storage/img/".$multimedia->id).'>
 				    </td>
 				    <td>
 						'.$descripcion.'
@@ -141,12 +137,10 @@ border-radius: 7px;color:white;"><i class="fa fa-trash"></i> Eliminar</span>
     		//console.log($(this).parent().parent('tr').find('#id').text());
     		alertify.confirm("Desea eliminar el archivo con id "+id+"?",
 			  function(){
-			  	var url = window.location.pathname.split('/');
-		        url = '/'+url[1]+'/'+url[2]+'/';
 
 		        $.ajax({
 		            type: 'POST',
-		            url: url+'eliminarDocumentacion',
+		            url: "{{ url('/eliminarDocumentacion') }}",
 		            dataType: "json",
 		            data : {'id' : id }
 		        }).done(function (data) {
