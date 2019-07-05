6f1cf31703abef72980a1c6841714f397f5f8dd6
hhx999 <borjas.vegariveras@gmail.com>
2019-07-05 13:15:02 -0300

    TG-220 #closed Migración de bd actualizada

--- database/migrations/2019_06_11_162854_create_USUARIOS_table.php
+++ database/migrations/2019_06_11_162854_create_USUARIOS_table.php
@@ -15,10 +15,16 @@ class CreateUSUARIOSTable extends Migration {
 		Schema::create('USUARIOS', function(Blueprint $table)
 		{
 			$table->integer('id', true);
-			$table->integer('dni')->unique('dni');
-			$table->string('nombre', 20);
+			$table->integer('cuit')->unique('cuit');
 			$table->string('password', 255);
+			$table->string('nombreApellido', 20);
+			$table->date('fecNacimiento');
+			$table->string('domicilio', 45);
 			$table->string('email', 45);
+			$table->string('localidad', 45);
+			$table->string('provincia', 45);
+			$table->string('agencia', 45);
+			$table->string('telefono', 45);
 			$table->boolean('verificado')->default(0);
 			$table->string('rol', 20);
 			$table->date('updated_at')->nullable();
