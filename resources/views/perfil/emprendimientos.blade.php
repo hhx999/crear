8dd5811d911d18f8b4b537ee9946d33431b35d1f
hhx999 <borjas.vegariveras@gmail.com>
2019-07-16 13:15:05 -0300

    TG-227 #closed Vistas para emprendimientos agregadas

--- resources/views/perfil/emprendimientos.blade.php
+++ resources/views/perfil/emprendimientos.blade.php
@@ -7,6 +7,35 @@
 	<header class="w3-container" style="padding-top:22px">
 	    <h5><b><i class="fa fa-dashboard"></i> Mis emprendimientos</b></h5>
 	  </header>
-
+	  <div class="w3-container">
+	  	<div class="w3-panel">
+	  		<div class="w3-row-padding" style="margin:0 -16px">
+		      <div class="w3-third">
+		        <button class="w3-button w3-green">Registrar un nuevo emprendimiento</button>
+		      </div>
+		      <div class="w3-twothird">
+			  <table class="w3-table">
+			    <tr>
+			      <th>CUIT</th>
+			      <th>Nombre del emprendimiento</th>
+			      <th>Nombre del encargado</th>
+			      <th>Acciones</th>
+			    </tr>
+			    <tr>
+			      <td>20-39355458-5</td>
+			      <td>Emprendimiento frutícola</td>
+			      <td>Vega Riveras, Borjas</td>
+			      <td>
+			      	<select style="color: black;">
+			      		<option>-</option>
+			      		<option>Editar</option>
+			      		<option>Eliminar</option>
+			      	</select>
+			      </td>
+			    </tr>
+			  </table>
+			 </div>
+			</div>
+		</div>
 
 	@endsection
