6989142f7c955b0979fc0c45167cff2c4392c12e
hhx999 <borjas.vegariveras@gmail.com>
2019-08-30 12:42:47 -0300

    TG-276 #done Constantes OK

---
+++ app/Http/Controllers/FinanciamientoController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+use App\F_CuestionarioLinea;
+use App\Formulario;
+use App\FormTipo;
+use App\Helpers;
+use App\Config;
+
+class FinanciamientoController extends Controller
+{
+    //
+    function creditos()
+    {
+    	return view('userTest.credito');
+    }
+    function cuestionarioCreditos(Request $request)
+    {
+    	$opcionesCuestionario = DB::table('f__cuestionario_lineas')
+				->where('estado','=', intval($request->estado))
+			    ->where('antiguedad', '<=', intval($request->antiguedad))
+			    ->whereIn('destino', $request->destino)
+			    ->distinct()
+			    ->get('form_tipo_id');
+
+		foreach ($opcionesCuestionario as $opcion) {
+			$lineas[] = FormTipo::find($opcion->form_tipo_id);
+		}
+
+		return view('userTest.credito', ['lineas_principales' => $lineas]);
+    }
+    function ingresarLineaEmprendedor(Request $request)
+    {
+    	if ($request->isMethod('post'))
+		{
+			//id de usuario
+			$idUsuario = $request->session()->get('id_usuario');
+			//el tipo de formulario es 1(linea emprendedor)
+			$idForm = 1;
+			//Creación del formulario
+			$formulario = new Formulario;
+
+
+			$estadosValidos = config('constantes.estadosIngresoForm');
+			if (array_key_exists($request->estado, $estadosValidos)) {
+				//Agregamos los parametros faltantes al request
+				$request->request->add(['idUsuario' => $idUsuario, 'form_tipo_id' => $idForm, 'estado' => $estadosValidos[$request->estado]]);
+				$formulario->create($request->all());
+				return $request->all();
+			} else {
+				return "Estado desconocido por sistema.";
+			}
+		}
+    	return view('financiamiento.ingresarLineaEmprendedor');
+    }
+}
