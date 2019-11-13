bb75ed3a101b7c92f378fed218e138c7f5faaf3f
hhx999 <borjas.vegariveras@gmail.com>
2019-11-13 12:58:58 -0300

    TG-371 #closed Tests subidos nuevamente

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -23,37 +23,4 @@ class UserTest extends TestCase
 
         $response->assertStatus(200);
     }
-    public function testLogin()
-    {
-        $this->get('/')->assertSee('INGRESO');
-        $datos = [
-            "dni" => "777",
-            "password" => "1234"
-        ];
-
-        $response = $this->post('/', $datos);
-        $response->assertRedirect('/admin');
-    }
-    public function testUsuarioIncorrecto()
-    {
-        $this->get('/')->assertSee('INGRESO');
-        $datos = [
-            "dni" => "+++",
-            "password" => "1234"
-        ];
-
-        $response = $this->post('/', $datos);
-        $response->assertSee('USUARIO INCORRECTO');
-    }
-    public function testPassIncorrecto()
-    {
-        $this->get('/')->assertSee('INGRESO');
-        $datos = [
-            "dni" => "777",
-            "password" => "23123"
-        ];
-
-        $response = $this->post('/', $datos);
-        $response->assertSee('PASSWORD INCORRECTO');
-    }
 }
