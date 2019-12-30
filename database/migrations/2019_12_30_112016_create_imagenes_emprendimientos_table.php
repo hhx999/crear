a8b19038af0bbc38b378f623aefa9862f463846a
hhx999 <borjas.vegariveras@gmail.com>
2019-12-30 11:29:11 -0300

    TG-407 #closed Emprendimientos comerciales junto con sus imagenes

---
+++ database/migrations/2019_12_30_112016_create_imagenes_emprendimientos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImagenesEmprendimientosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('imagenes_emprendimientos', function (Blueprint $table) {
+            $table->integer('id', true);
+            $table->string('descripcion');
+            $table->integer('emprendimiento_comercial_id')->index('emprendimiento_comercial_id');
+            $table->integer('multimedia_id')->index('multimedia_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('imagenes_emprendimientos');
+    }
+}
---
+++ database/migrations/2019_12_30_112204_add_foreign_key_imagenes_emprendimientos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddForeignKeyImagenesEmprendimientosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('imagenes_emprendimientos', function (Blueprint $table) {
+            //
+            $table->foreign('emprendimiento_comercial_id', 'ImagenesEmprendimiento')->references('id')->on('emprendimiento_comerciales')->onUpdate('RESTRICT')->onDelete('RESTRICT');
+            $table->foreign('multimedia_id', 'ImagenesMultimedia')->references('id')->on('MULTIMEDIA')->onUpdate('RESTRICT')->onDelete('RESTRICT');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('imagenes_emprendimientos', function (Blueprint $table) {
+            //
+            $table->dropForeign('ImagenesEmprendimiento');
+            $table->dropForeign('ImagenesMultimedia');
+        });
+    }
+}
